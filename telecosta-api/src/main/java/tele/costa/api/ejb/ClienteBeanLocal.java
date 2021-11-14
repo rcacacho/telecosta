af88da1fe16000405c6b8c4a4534aeea7bebc767
rcacacho <estuardo.cacacho@gmail.com>
2021-11-13 21:51:38 -0600

    registro de cliente

---
+++ telecosta-api/src/main/java/tele/costa/api/ejb/ClienteBeanLocal.java
@@ -0,0 +1,24 @@
+package tele.costa.api.ejb;
+
+import java.util.List;
+import tele.costa.api.entity.Cliente;
+
+/**
+ *
+ * @author elfo_
+ */
+public interface ClienteBeanLocal {
+
+    List<Cliente> ListClientes();
+
+    Cliente saveCliente(Cliente cliente);
+
+    Cliente updateCliente(Cliente cliente);
+
+    List<Cliente> ListClientesByNombre(String nombre);
+
+    List<Cliente> ListClientesByCui(String cui);
+
+    Cliente findClienteById(Integer idCliente);
+
+}
