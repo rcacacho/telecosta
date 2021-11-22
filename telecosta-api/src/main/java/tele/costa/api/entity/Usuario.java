3eafcefecd7b6e7426c34b98aa8208da8089fb38
rcacacho <estuardo.cacacho@gmail.com>
2021-11-21 21:14:04 -0600

    creacion de usuario

--- telecosta-api/src/main/java/tele/costa/api/entity/Usuario.java
+++ telecosta-api/src/main/java/tele/costa/api/entity/Usuario.java
@@ -1,6 +1,7 @@
 package tele.costa.api.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -38,36 +41,56 @@ public class Usuario implements Serializable {
     @Basic(optional = false)
     @Column(name = "idusuario")
     private Integer idusuario;
-    
+
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 100)
     @Column(name = "nombres")
     private String nombres;
-    
+
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 100)
     @Column(name = "apellidos")
     private String apellidos;
-    
+
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 50)
     @Column(name = "usuario")
     private String usuario;
-    
+
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 50)
     @Column(name = "password")
     private String password;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "root")
     private boolean root;
-    
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "usuariocreacion")
+    private String usuariocreacion;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fechacreacion")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechacreacion;
+
+    @Size(max = 50)
+    @Column(name = "usuariomodificacion")
+    private String usuariomodificacion;
+
+    @Column(name = "fechamodificacion")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechamodificacion;
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "activo")
@@ -141,9 +164,41 @@ public class Usuario implements Serializable {
     public boolean getActivo() {
         return activo;
     }
-    
+
     public void setActivo(boolean activo) {
         this.activo = activo;
+    }
+
+    public String getUsuariocreacion() {
+        return usuariocreacion;
+    }
+
+    public void setUsuariocreacion(String usuariocreacion) {
+        this.usuariocreacion = usuariocreacion;
+    }
+
+    public Date getFechacreacion() {
+        return fechacreacion;
+    }
+
+    public void setFechacreacion(Date fechacreacion) {
+        this.fechacreacion = fechacreacion;
+    }
+
+    public String getUsuariomodificacion() {
+        return usuariomodificacion;
+    }
+
+    public void setUsuariomodificacion(String usuariomodificacion) {
+        this.usuariomodificacion = usuariomodificacion;
+    }
+
+    public Date getFechamodificacion() {
+        return fechamodificacion;
+    }
+
+    public void setFechamodificacion(Date fechamodificacion) {
+        this.fechamodificacion = fechamodificacion;
     }
 
     @Override
@@ -170,5 +225,5 @@ public class Usuario implements Serializable {
     public String toString() {
         return "tele.costa.api.entity.Usuario[ idusuario=" + idusuario + " ]";
     }
-    
+
 }
