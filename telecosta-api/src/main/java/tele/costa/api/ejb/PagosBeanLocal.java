ceb331b450ee0781689a9a7acb0f9121bc2dbdce
rcacacho <estuardo.cacacho@gmail.com>
2021-12-12 19:12:28 -0600

    reporte de cobros

--- telecosta-api/src/main/java/tele/costa/api/ejb/PagosBeanLocal.java
+++ telecosta-api/src/main/java/tele/costa/api/ejb/PagosBeanLocal.java
@@ -9,28 +9,29 @@ import tele.costa.api.entity.Pago;
  * @author rcacacho
  */
 public interface PagosBeanLocal {
-    
+
     List<Pago> listPagos(Date fechainicio, Date fechafin);
-    
+
     List<Pago> listCobros(Date fechainicio, Date fechafin);
 
     Pago savePago(Pago pago);
 
     Pago saveCobro(Pago pago);
-    
-    Pago findPagoByIdClienteAndAnioAndMes (Integer idCliente, Integer anio, String mes);
-    
+
+    Pago findPagoByIdClienteAndAnioAndMes(Integer idCliente, Integer anio, String mes);
+
     List<Pago> listPagoByIdCliente(Integer idcliente);
-    
+
     List<Pago> listPagoByAnio(Integer anio);
-    
+
     List<Pago> listPagoByMes(String mes);
-    
-     List<Pago> listCobroByIdCliente(Integer idcliente);
-    
+
+    List<Pago> listCobroByIdCliente(Integer idcliente);
+
     List<Pago> listCobroByAnio(Integer anio);
-    
+
     List<Pago> listCobroByMes(String mes);
+
+    Pago findPagoByIdPago(Integer idPago);
     
-     Pago findPagoByIdPago (Integer idPago);
 }
