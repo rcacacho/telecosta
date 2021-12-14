5caf7c7499a5702e1cf56463bb0dd068edeca6f1
rcacacho <estuardo.cacacho@gmail.com>
2021-12-13 21:01:22 -0600

    configuracion

--- telecosta-api/src/main/java/tele/costa/api/ejb/ConfiguracionBeanLocal.java
+++ telecosta-api/src/main/java/tele/costa/api/ejb/ConfiguracionBeanLocal.java
@@ -1,14 +1,24 @@
 package tele.costa.api.ejb;
 
 import java.util.List;
+import javax.ejb.Local;
 import tele.costa.api.entity.Configuracionpago;
 
 /**
  *
  * @author rcacacho
  */
+@Local
 public interface ConfiguracionBeanLocal {
 
     List<Configuracionpago> listConfiguracionPago();
+
+    Configuracionpago saveConfiguracionPago(Configuracionpago configuracion);
+
+    Configuracionpago deleteConfiguracionPago(Integer idconfiguracionpago);
+
+    Configuracionpago findConfiguracionPago(Integer idconfiguracionpago);
+
+    Configuracionpago actualizarConfiguracion(Configuracionpago configuracion);
 
 }
