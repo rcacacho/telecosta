dffd3256b05b8b42b8abdd6dd5c9d4b7dcaadd57
rcacacho <estuardo.cacacho@gmail.com>
2021-12-16 20:50:32 -0600

    compras

--- telecosta-bussines/src/main/java/tele/costa/bussines/bussines/ejb/imp/ComprasBean.java
+++ telecosta-bussines/src/main/java/tele/costa/bussines/bussines/ejb/imp/ComprasBean.java
@@ -1,14 +1,18 @@
 package tele.costa.bussines.bussines.ejb.imp;
 
+import java.util.Date;
 import java.util.List;
 import javax.annotation.Resource;
 import javax.ejb.EJBContext;
 import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import org.apache.log4j.Logger;
 import tele.costa.api.ejb.ComprasBeanLocal;
 import tele.costa.api.entity.Compra;
+import tele.costa.api.entity.Proveedor;
 
 /**
  *
@@ -29,6 +33,18 @@ public class ComprasBean implements ComprasBeanLocal {
         log.error(ex.getMessage(), ex);
     }
 
+    private String getConstraintViolationExceptionAsString(ConstraintViolationException ex) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Error de validación:\n");
+        for (ConstraintViolation c : ex.getConstraintViolations()) {
+            sb.append(String.format("[bean: %s; field: %s; message: %s; value: %s]",
+                    c.getRootBeanClass().getName(),
+                    c.getPropertyPath().toString(),
+                    c.getMessage(), c.getInvalidValue()));
+        }
+        return sb.toString();
+    }
+
     @Override
     public List<Compra> listCompra() {
         List<Compra> lst = em.createQuery("SELECT qj FROM Compra qj where qj.activo = true ", Compra.class)
@@ -39,6 +55,74 @@ public class ComprasBean implements ComprasBeanLocal {
         }
 
         return lst;
+    }
+
+    @Override
+    public Compra saveCompra(Compra compra) {
+        try {
+            compra.setActivo(true);
+            compra.setFechacreacion(new Date());
+            em.persist(compra);
+            em.flush();
+            return (compra);
+        } catch (ConstraintViolationException ex) {
+            String validationError = getConstraintViolationExceptionAsString(ex);
+            log.error(validationError);
+            context.setRollbackOnly();
+            return null;
+        } catch (Exception ex) {
+            processException(ex);
+            context.setRollbackOnly();
+            return null;
+        }
+    }
+
+    @Override
+    public Compra findCompraById(Integer idcompra) {
+        if (idcompra == null) {
+            return null;
+        }
+
+        List<Compra> lst = em.createQuery("SELECT pa FROM Compra pa WHERE pa.idcompra =:idcompra ", Compra.class)
+                .setParameter("idcompra", idcompra)
+                .getResultList();
+
+        if (lst == null || lst.isEmpty()) {
+            return null;
+        }
+        return lst.get(0);
+    }
+
+    @Override
+    public List<Proveedor> listProveedor() {
+        List<Proveedor> lst = em.createQuery("SELECT qj FROM Proveedor qj where qj.activo = true ", Proveedor.class)
+                .getResultList();
+
+        if (lst == null || lst.isEmpty()) {
+            return null;
+        }
+
+        return lst;
+    }
+
+    @Override
+    public Proveedor saveProveedor(Proveedor proveedor) {
+        try {
+            proveedor.setActivo(true);
+            proveedor.setFechacreacion(new Date());
+            em.persist(proveedor);
+            em.flush();
+            return (proveedor);
+        } catch (ConstraintViolationException ex) {
+            String validationError = getConstraintViolationExceptionAsString(ex);
+            log.error(validationError);
+            context.setRollbackOnly();
+            return null;
+        } catch (Exception ex) {
+            processException(ex);
+            context.setRollbackOnly();
+            return null;
+        }
     }
 
 }
--- telecosta-bussines/src/main/java/tele/costa/bussines/bussines/ejb/imp/ConfiguracionBean.java
+++ telecosta-bussines/src/main/java/tele/costa/bussines/bussines/ejb/imp/ConfiguracionBean.java
@@ -12,6 +12,7 @@ import javax.validation.ConstraintViolationException;
 import org.apache.log4j.Logger;
 import tele.costa.api.ejb.ConfiguracionBeanLocal;
 import tele.costa.api.entity.Configuracionpago;
+import tele.costa.api.entity.Tipocompra;
 
 /**
  *
@@ -19,19 +20,19 @@ import tele.costa.api.entity.Configuracionpago;
  */
 @Singleton
 public class ConfiguracionBean implements ConfiguracionBeanLocal {
-    
+
     private static final Logger log = Logger.getLogger(ConfiguracionBean.class);
-    
+
     @PersistenceContext(unitName = "TeleCostaPU")
     EntityManager em;
-    
+
     @Resource
     private EJBContext context;
-    
+
     private void processException(Exception ex) {
         log.error(ex.getMessage(), ex);
     }
-    
+
     private String getConstraintViolationExceptionAsString(ConstraintViolationException ex) {
         StringBuilder sb = new StringBuilder();
         sb.append("Error de validación:\n");
@@ -43,19 +44,19 @@ public class ConfiguracionBean implements ConfiguracionBeanLocal {
         }
         return sb.toString();
     }
-    
+
     @Override
     public List<Configuracionpago> listConfiguracionPago() {
         List<Configuracionpago> lst = em.createQuery("SELECT qj FROM Configuracionpago qj where qj.activo = true ", Configuracionpago.class)
                 .getResultList();
-        
+
         if (lst == null || lst.isEmpty()) {
             return null;
         }
-        
+
         return lst;
     }
-    
+
     @Override
     public Configuracionpago saveConfiguracionPago(Configuracionpago configuracion) {
         try {
@@ -75,22 +76,22 @@ public class ConfiguracionBean implements ConfiguracionBeanLocal {
             return null;
         }
     }
-    
+
     @Override
     public Configuracionpago deleteConfiguracionPago(Integer idconfiguracionpago, String usuario) {
         if (idconfiguracionpago == null) {
             context.setRollbackOnly();
             return null;
         }
-        
+
         try {
             Configuracionpago toUpdate = em.find(Configuracionpago.class, idconfiguracionpago);
-            
+
             toUpdate.setUsuarioeliminacion(usuario);
             toUpdate.setFechaeliminacion(new Date());
             toUpdate.setActivo(false);
             em.merge(toUpdate);
-            
+
             return toUpdate;
         } catch (ConstraintViolationException ex) {
             String validationError = getConstraintViolationExceptionAsString(ex);
@@ -102,37 +103,37 @@ public class ConfiguracionBean implements ConfiguracionBeanLocal {
             return null;
         }
     }
-    
+
     @Override
     public Configuracionpago findConfiguracionPago(Integer idconfiguracionpago) {
         if (idconfiguracionpago == null) {
             return null;
         }
-        
+
         List<Configuracionpago> lst = em.createQuery("SELECT col FROM Configuracionpago col WHERE col.idconfiguracionpago =:idconfiguracionpago ", Configuracionpago.class)
                 .setParameter("idconfiguracionpago", idconfiguracionpago)
                 .getResultList();
-        
+
         if (lst == null || lst.isEmpty()) {
             return null;
         }
         return lst.get(0);
     }
-    
+
     @Override
     public Configuracionpago actualizarConfiguracion(Configuracionpago configuracion) {
         if (configuracion == null) {
             context.setRollbackOnly();
             return null;
         }
-        
+
         try {
             Configuracionpago toUpdate = em.find(Configuracionpago.class, configuracion.getIdconfiguracionpago());
-            
+
             toUpdate.setDescripcion(configuracion.getDescripcion());
             toUpdate.setValor(configuracion.getValor());
             em.merge(toUpdate);
-            
+
             return toUpdate;
         } catch (ConstraintViolationException ex) {
             String validationError = getConstraintViolationExceptionAsString(ex);
@@ -144,5 +145,106 @@ public class ConfiguracionBean implements ConfiguracionBeanLocal {
             return null;
         }
     }
-    
+
+    @Override
+    public List<Tipocompra> listConfiguracionGasto() {
+        List<Tipocompra> lst = em.createQuery("SELECT qj FROM Tipocompra qj where qj.activo = true ", Tipocompra.class)
+                .getResultList();
+
+        if (lst == null || lst.isEmpty()) {
+            return null;
+        }
+
+        return lst;
+    }
+
+    @Override
+    public Tipocompra saveConfiguracionGasto(Tipocompra configuracionGasto) {
+        try {
+            configuracionGasto.setActivo(true);
+            configuracionGasto.setFechacreacion(new Date());
+            em.persist(configuracionGasto);
+            em.flush();
+            return (configuracionGasto);
+        } catch (ConstraintViolationException ex) {
+            String validationError = getConstraintViolationExceptionAsString(ex);
+            log.error(validationError);
+            context.setRollbackOnly();
+            return null;
+        } catch (Exception ex) {
+            processException(ex);
+            context.setRollbackOnly();
+            return null;
+        }
+    }
+
+    @Override
+    public Tipocompra deleteConfiguracionGasto(Integer idtipocompra, String usuario) {
+        if (idtipocompra == null) {
+            context.setRollbackOnly();
+            return null;
+        }
+
+        try {
+            Tipocompra toUpdate = em.find(Tipocompra.class, idtipocompra);
+
+            toUpdate.setUsuariomodificacion(usuario);
+            toUpdate.setFechamodificacion(new Date());
+            toUpdate.setActivo(false);
+            em.merge(toUpdate);
+
+            return toUpdate;
+        } catch (ConstraintViolationException ex) {
+            String validationError = getConstraintViolationExceptionAsString(ex);
+            log.error(validationError);
+            context.setRollbackOnly();
+            return null;
+        } catch (Exception ex) {
+            processException(ex);
+            return null;
+        }
+    }
+
+    @Override
+    public Tipocompra findConfiguracionGasto(Integer idtipocompra) {
+        if (idtipocompra == null) {
+            return null;
+        }
+
+        List<Tipocompra> lst = em.createQuery("SELECT col FROM Tipocompra col WHERE col.idtipocompra =:idtipocompra ", Tipocompra.class)
+                .setParameter("idtipocompra", idtipocompra)
+                .getResultList();
+
+        if (lst == null || lst.isEmpty()) {
+            return null;
+        }
+        return lst.get(0);
+    }
+
+    @Override
+    public Tipocompra actualizarConfiguracionGasto(Tipocompra configuracionGasto) {
+        if (configuracionGasto == null) {
+            context.setRollbackOnly();
+            return null;
+        }
+
+        try {
+            Tipocompra toUpdate = em.find(Tipocompra.class, configuracionGasto.getIdtipocompra());
+
+            toUpdate.setDescripcion(configuracionGasto.getDescripcion());
+            toUpdate.setTipo(configuracionGasto.getTipo());
+            em.merge(toUpdate);
+
+            return toUpdate;
+        } catch (ConstraintViolationException ex) {
+            String validationError = getConstraintViolationExceptionAsString(ex);
+            log.error(validationError);
+            context.setRollbackOnly();
+            return null;
+        } catch (Exception ex) {
+            processException(ex);
+            return null;
+        }
+    }
+
 }
