be163ebd61951f9ef985bd9d6863d33e60d67cea
rcacacho <estuardo.cacacho@gmail.com>
2021-12-16 20:50:25 -0600

    compras

--- telecosta-api/src/main/java/tele/costa/api/ejb/ComprasBeanLocal.java
+++ telecosta-api/src/main/java/tele/costa/api/ejb/ComprasBeanLocal.java
@@ -3,6 +3,7 @@ package tele.costa.api.ejb;
 import java.util.List;
 import javax.ejb.Local;
 import tele.costa.api.entity.Compra;
+import tele.costa.api.entity.Proveedor;
 
 /**
  *
@@ -10,7 +11,15 @@ import tele.costa.api.entity.Compra;
  */
 @Local
 public interface ComprasBeanLocal {
-    
+
     List<Compra> listCompra();
-    
+
+    Compra saveCompra(Compra compra);
+
+    Compra findCompraById(Integer idcompra);
+
+    List<Proveedor> listProveedor();
+
+    Proveedor saveProveedor(Proveedor proveedor);
+
 }
--- telecosta-api/src/main/java/tele/costa/api/ejb/ConfiguracionBeanLocal.java
+++ telecosta-api/src/main/java/tele/costa/api/ejb/ConfiguracionBeanLocal.java
@@ -3,6 +3,7 @@ package tele.costa.api.ejb;
 import java.util.List;
 import javax.ejb.Local;
 import tele.costa.api.entity.Configuracionpago;
+import tele.costa.api.entity.Tipocompra;
 
 /**
  *
@@ -20,5 +21,15 @@ public interface ConfiguracionBeanLocal {
     Configuracionpago findConfiguracionPago(Integer idconfiguracionpago);
 
     Configuracionpago actualizarConfiguracion(Configuracionpago configuracion);
+
+    List<Tipocompra> listConfiguracionGasto();
+
+    Tipocompra saveConfiguracionGasto(Tipocompra configuracionGasto);
+
+    Tipocompra deleteConfiguracionGasto(Integer idtipocompra, String usuario);
+
+    Tipocompra findConfiguracionGasto(Integer idtipocompra);
+
+    Tipocompra actualizarConfiguracionGasto(Tipocompra configuracionGasto);
 
 }
