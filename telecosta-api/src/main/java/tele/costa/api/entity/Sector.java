2022fcbd5e6c75fa2a88695d6aeec68db2acc6a4
Raul Cacacho <estuardo.cacacho@gmail.com>
2022-03-24 13:27:26 -0600

    cobros

--- telecosta-api/src/main/java/tele/costa/api/entity/Sector.java
+++ telecosta-api/src/main/java/tele/costa/api/entity/Sector.java
@@ -2,9 +2,7 @@ package tele.costa.api.entity;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -15,14 +13,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
